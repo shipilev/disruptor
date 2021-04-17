3cb7905ac227c762cddadd8a747febeacc2a040e
github-action <github-action@github.com>
2021-04-17 18:48:44 +0000

    Simple GitHub Pages Deploy Action

--- src/main/java/com/lmax/disruptor/BatchStartAware.java
+++
@@ -1,9 +0,0 @@
-package com.lmax.disruptor;
-
-/**
- * Called by the {@link BatchEventProcessor} on start of processing a batch of events.
- */
-public interface BatchStartAware
-{
-    void onBatchStart(long batchSize);
-}
